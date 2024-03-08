8b647e7d88ec513b8f7566581dec1bd074f46e33
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-03-08 19:31:32 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"; 
+import { ModeToggle } from "@/components/global/mode-toggle";
+
+export default function Home() {
+  return (
+      <div className=""> 
+        </div>
+   );
+}
